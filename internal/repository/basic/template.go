f256163967dd24bf32bf8f5bbda7efe066fb7e15
TheDeveloper10 <th3r3ap3r@abv.bg>
2023-04-26 19:58:42 +0300

    Implemented all functions in template repository except 1

--- internal/repository/basic/template.go
+++ internal/repository/basic/template.go
@@ -15,13 +15,11 @@ func (tr *TemplateRepository) CreateTemplate(template *dto.Template) (uint64, ut
 		template.Body.Email, template.Body.SMS, template.Body.Push, template.Language, template.Type,
 	)
 	if err != nil {
-		util.Logger.Info().Msg(err.Error())
 		return 0, util.StatusError
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		util.Logger.Info().Msg(err.Error())
 		return 0, util.StatusError
 	}
 
@@ -29,11 +27,52 @@ func (tr *TemplateRepository) CreateTemplate(template *dto.Template) (uint64, ut
 }
 
 func (tr *TemplateRepository) UpdateTemplate(templateID uint64, template *dto.Template) util.StatusCode {
+	result, err := client.Database.Exec(
+		"update templates set bodyEmail=?, bodySMS=?, bodyPush=?, language=?, type=? where id=?",
+		template.Body.Email, template.Body.SMS, template.Body.Push, template.Language, template.Type, templateID,
+	)
+	if err != nil {
+		return util.StatusError
+	}
+
+	affectedRows, err := result.RowsAffected()
+	if err != nil {
+		return util.StatusError
+	} else if affectedRows <= 0 {
+		return util.StatusNotFound
+	}
+
 	return util.StatusSuccess
 }
 
 func (tr *TemplateRepository) GetTemplateByID(templateID uint64) (*dto.Template, util.StatusCode) {
-	return nil, util.StatusSuccess
+	rows, err := client.Database.Query(
+		"select bodyEmail, bodySMS, bodyPush, language, type from templates where id=?",
+		templateID,
+	)
+	if err != nil {
+		return nil, util.StatusError
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		template := dto.Template{Body: dto.TemplateBody{}}
+
+		err := rows.Scan(
+			&template.Body.Email,
+			&template.Body.SMS,
+			&template.Body.Push,
+			&template.Language,
+			&template.Type,
+		)
+		if err != nil {
+			return nil, util.StatusError
+		}
+
+		return &template, util.StatusSuccess
+	}
+
+	return nil, util.StatusNotFound
 }
 
 func (tr *TemplateRepository) GetBulkTemplates(filter *dto.TemplateBulkFilter) ([]dto.Template, util.StatusCode) {
@@ -41,5 +80,20 @@ func (tr *TemplateRepository) GetBulkTemplates(filter *dto.TemplateBulkFilter) (
 }
 
 func (tr *TemplateRepository) DeleteTemplate(templateID uint64) util.StatusCode {
+	result, err := client.Database.Exec(
+		"delete from templates where id=?",
+		templateID,
+	)
+	if err != nil {
+		return util.StatusError
+	}
+
+	affectedRows, err := result.RowsAffected()
+	if err != nil {
+		return util.StatusError
+	} else if affectedRows <= 0 {
+		return util.StatusNotFound
+	}
+
 	return util.StatusSuccess
 }
