b570e19626a3e0b9843636a60be042d5876b9b7c
TheDeveloper10 <th3r3ap3r@abv.bg>
2023-04-29 16:40:33 +0300

    Implemented realSMS client and added logging to notification sender repository

--- internal/client/realSMS.go
+++ internal/client/realSMS.go
@@ -1,14 +1,49 @@
 package client
 
-import "notification-service/internal/config"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"notification-service/internal/config"
+	"strings"
+)
 
 func newRealSMSClientFromConfig(conf *config.SMSConfig) *realSMS {
-	return &realSMS{}
+	return &realSMS{
+		httpClient:        &http.Client{},
+		endpoint:          fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json", conf.AccountSID),
+		queryParamsFormat: fmt.Sprintf("MessagingServiceSid=%s&To=%%s&Body=%%s", conf.MessagingServiceSID),
+		accountSID:        conf.AccountSID,
+		authToken:         conf.AuthToken,
+	}
 }
 
 type realSMS struct {
+	httpClient        *http.Client
+	endpoint          string
+	queryParamsFormat string
+
+	accountSID string
+	authToken  string
 }
 
 func (rs *realSMS) SendSMS(title string, body string, to string) error {
+	message := fmt.Sprintf("%s\r\n%s", title, body)
+	relativeParameters := fmt.Sprintf(rs.queryParamsFormat, to, message)
+	data := strings.NewReader(relativeParameters)
+
+	req, err := http.NewRequest("POST", rs.endpoint, data)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(rs.accountSID, rs.authToken)
+	resp, err := rs.httpClient.Do(req)
+	if err != nil {
+		return err
+	} else if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return errors.New("failed to send SMS")
+	}
+
 	return nil
 }
--- internal/repository/basic/notificationSender.go
+++ internal/repository/basic/notificationSender.go
@@ -11,27 +11,24 @@ type NotificationSenderRepository struct {
 
 func (nsr *NotificationSenderRepository) SendEmail(notification *dto.Notification) util.StatusCode {
 	err := client.Mail.MailSingle(notification.Title, notification.Message, notification.ContactInfo)
-	if err == nil {
-		return util.StatusSuccess
-	} else {
-		return util.StatusError
-	}
+	return nsr.handleError(err)
 }
 
 func (nsr *NotificationSenderRepository) SendSMS(notification *dto.Notification) util.StatusCode {
 	err := client.SMS.SendSMS(notification.Title, notification.Message, notification.ContactInfo)
-	if err == nil {
-		return util.StatusSuccess
-	} else {
-		return util.StatusError
-	}
+	return nsr.handleError(err)
 }
 
 func (nsr *NotificationSenderRepository) SendPush(notification *dto.Notification) util.StatusCode {
 	err := client.Push.SendMessage(notification.Title, notification.Message, notification.ContactInfo)
+	return nsr.handleError(err)
+}
+
+func (nsr *NotificationSenderRepository) handleError(err error) util.StatusCode {
 	if err == nil {
 		return util.StatusSuccess
 	} else {
+		util.Logger.Error().Msg(err.Error())
 		return util.StatusError
 	}
 }
